b915758cc60ab4e4abfa4ac90536738bb587df59
sarghiniyasser <sarghiniyasser@gmail.com>
2023-02-22 11:29:54 +0100

    loginn3

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
-
+import MainPage from './Pages/MainPage';
 import './App.css';
 import Register from './Pages/Register';
 import Login from './Pages/Login';
@@ -11,6 +11,8 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/mainpage" element={<MainPage />} />
+      
     </Routes>
   </BrowserRouter>
   )
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,15 +1,29 @@
 import React from "react";
-
-
-const Register = () =>{
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';const Login = () =>{
     return(
         <>
-        <div className="flex">
-            Register
-        </div>
+        <h1>Register</h1>
+        <label>nombre usuario</label>
+        <input id="username" type="text" style={{background: 'gray'}}></input>
+        <label>correo</label>
+        <input id="email" type="email" style={{background: 'gray'}}></input>
+        <label>contraseña</label>
+        <input id="password" type="password" style={{background: 'gray'}}></input>
+        <label>Repetir contraseña</label>
+        <input id="password2" type="password" style={{background: 'gray'}}></input>
+        <button style={{background: 'green', marginLeft: '20px'}}>Sumbit</button>
+        <br></br>
+        <div style={{marginTop: '30px', padding: '20px'}}>
+        <button style={{background: 'gray'}}>Google</button>
+        <button style={{background: 'blue'}}>Fcebook</button>
+        <button style={{background: 'black', color: 'white'}}>Hithub</button>
+
         
+            <Link to = "/"><button style={{marginLeft: '50px', background: 'purple'}}>LoginPage</button></Link>
+        </div>
         </>
     )
 }
 
-export default Register;
+
+export default Login;
