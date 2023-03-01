c385bd61da3993d10734a9a619a9ff19fee0a3be
gabriel-nakamura <gabrielnakamurasuero@gmail.com>
2023-03-01 11:39:09 +0100

    add login & register

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,9 +1,5 @@
 import React, { useState } from "react";
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
-<<<<<<< HEAD
-
-function Register() {
-=======
 import firebase from 'firebase/compat/app';
 import { auth, db } from "../firebase.js";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
@@ -12,49 +8,11 @@ import { checkIfEmail, checkIfNotEmpty } from "../common";
 const Register = () => {
 
     const authForRegister = getAuth();
->>>>>>> origin/login
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [password2, setPassword2] = useState("");
     const [passwordValid, setPasswordValid] = useState(false);
-<<<<<<< HEAD
-  
-    const handlePasswordChange = (event) => {
-      const passwordValue = event.target.value;
-      setPassword(passwordValue);
-  
-      // Validar la contraseña
-      const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
-      setPasswordValid(passwordValue === "" || passwordRegex.test(passwordValue));
-    }
-  
-    return (
-      <>
-          <h1>Register</h1>
-      <label>Nombre de usuario:</label>
-      <input id="username" type="text" style={{background: 'gray'}} value={username} onChange={(e) => setUsername(e.target.value)} />
-      <label>Correo:</label>
-      <input id="email" type="email" style={{background: 'gray'}} value={email} onChange={(e) => setEmail(e.target.value)} />
-      <label>Contraseña:</label>
-      <input id="password" type="password" style={{background: 'gray'}} value={password} onChange={handlePasswordChange} />
-      {!passwordValid && password !== "" && <div style={{color: 'red'}}>La contraseña es demasiado débil</div>}
-      <label>Repetir contraseña:</label>
-      <input id="password2" type="password" style={{background: 'gray'}} value={password2} onChange={(e) => setPassword2(e.target.value)} />
-      <button style={{background: 'green', marginLeft: '20px'}}>Sumbit</button>
-      <br />
-      <div style={{marginTop: '30px', padding: '20px'}}>
-        <button style={{background: 'gray'}}>Google</button>
-        <button style={{background: 'blue'}}>Facebook</button>
-        <button style={{background: 'black', color: 'white'}}>Github</button>
-        <Link to = "/"><button style={{marginLeft: '50px', background: 'purple'}}>LoginPage</button></Link>
-      </div>
-      </>
-    );
-  }
-  
-  export default Register;
-=======
     
     /**
      * Registrar a la aplicación
@@ -128,4 +86,3 @@ const Register = () => {
 
 
 export default Register;
->>>>>>> origin/login
